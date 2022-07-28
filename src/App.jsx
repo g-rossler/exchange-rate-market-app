0bb6d3f093cc1b4aa830f20a0f6e49dbe30f5e51
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-07-28 20:40:01 -0300

    Add Currency calculator component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
   Stack,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 import Navbar from './components/Navbar';
+import CurrencyCalculator from './components/Currency-calculator';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Stack p={2}>
+      <Stack p={2} align='center' justify='center'>
         <Navbar />
+        <CurrencyCalculator />
       </Stack>
     </ChakraProvider>
   );
