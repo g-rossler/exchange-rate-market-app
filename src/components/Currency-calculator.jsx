21550913e4eec3ca68dbc9f2975cd63d4e95ac57
Rossler Gerardo <60127824+g-rossler@users.noreply.github.com>
2022-09-02 12:00:51 -0300

    Revert "Add handleInput from user"
    This reverts commit ccd60ed036515b9bbb614b1ce9bd87de64b2914d.

--- src/components/Currency-calculator.jsx
+++ src/components/Currency-calculator.jsx
@@ -23,29 +23,11 @@ import {
   Text,
   Box,
 } from '@chakra-ui/react';
-import useFetchData from '../hooks/useFetchData';
 
 function CurrencyCalculator() {
-  const [inputUser, setInputUser] = useState({
-    amount: "",
-    baseCurrency: "USD",
-    futureCurrency: "AUD"
-  })
-  //const isErrorAmountInput = input === '3';
-
-  function handleInputChange(e) {
-    const {name, value} = e.target
-      setInputUser(prevInputUser => {
-        return {
-          ...prevInputUser,
-          [name]: value
-        }
-      })
-  }
-
-  function fetchData(){
-    useFetchData(inputUser)
-  }
+  const [input, setInput] = useState('');
+  const handleInputChange = e => setInput(e.target.value);
+  const isErrorAmountInput = input === '3';
 
   return (
     <Stack
@@ -68,19 +50,22 @@ function CurrencyCalculator() {
         spacing={10}
       >
         <VStack minH="110px">
-          <FormControl>
+          <FormControl isInvalid={isErrorAmountInput}>
             <FormLabel textAlign="center">Amount:</FormLabel>
             <InputGroup>
               <InputLeftAddon children="$" minH="50px" />
               <Input
                 type="text"
-                onChange={(e) => handleInputChange(e)}
-                value={inputUser.amount}
+                onChange={handleInputChange}
+                value={input}
                 minH="50px"
-                name='amount'
               />
             </InputGroup>
-           
+            {!isErrorAmountInput ? (
+              <FormHelperText>Enter an amount greater than 0.</FormHelperText>
+            ) : (
+              <FormErrorMessage>Enter a valid number.</FormErrorMessage>
+            )}
           </FormControl>
         </VStack>
 
@@ -95,7 +80,7 @@ function CurrencyCalculator() {
             minW="290px"
           >
             <Image src={flagUSD} boxSize="20px" />
-            <Select variant="unstyled" name='baseCurrency' onChange={(e) => handleInputChange(e)}>
+            <Select variant="unstyled">
               <option value="USD">USD - US Dollar</option>
               <option value="AUD">AUD - Australian Dollar</option>
               <option value="CAD">CAD - Canadian Dollar</option>
@@ -126,8 +111,8 @@ function CurrencyCalculator() {
             borderColor="gray.200"
             minW="290px"
           >
-            <Image src={flagAUD} boxSize="20px"/>
-            <Select variant="unstyled" name='futureCurrency' onChange={(e) => handleInputChange(e)}>
+            <Image src={flagAUD} boxSize="20px" />
+            <Select variant="unstyled">
               <option value="AUD">AUD - Australian Dollar</option>
               <option value="USD">USD - US Dollar</option>
               <option value="CAD">CAD - Canadian Dollar</option>
@@ -139,7 +124,7 @@ function CurrencyCalculator() {
       </Stack>
 
       <Stack align="center" justify="center">
-        <Button maxW={48} onClick={() => fetchData()}>Convert Now!</Button>
+        <Button maxW={48}>Convert Now!</Button>
       </Stack>
     </Stack>
   );
