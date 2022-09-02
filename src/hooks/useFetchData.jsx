40c686a19816f34f3c373d86e28764f861b7b89d
Rossler Gerardo <60127824+g-rossler@users.noreply.github.com>
2022-09-02 12:00:51 -0300

    Revert "Create hook fetch data"
    This reverts commit 87663d45e23dc91dd42385b02997cd80df788e35.

--- src/hooks/useFetchData.jsx
+++
@@ -1,8 +0,0 @@
-function useFetchData(userInput) {
-  const {amount, baseCurrency, futureCurrency} = userInput
-  const BASE_URL = 'https://api.exchangerate.host/latest';
-  console.log(amount, baseCurrency, futureCurrency, BASE_URL)
-
-}
-
-export default useFetchData
