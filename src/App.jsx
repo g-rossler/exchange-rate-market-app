26e25b4954c533844efcfe50e72d9cc640d8ab4f
Rossler Gerardo <60127824+g-rossler@users.noreply.github.com>
2022-09-02 12:00:51 -0300

    Revert "Add React Router"
    This reverts commit 070df3feccd4eeba7335148c81057da6a38d5d50.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { ChakraProvider, theme, Stack } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  theme,
+  Stack,
+} from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 import CurrencyCalculator from './components/CurrencyCalculator';
-import HistoricCurrency from './components/HistoricCurrency';
-import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Stack p={2} align="center" justify="center">
+      <Stack p={2} align='center' justify='center'>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<CurrencyCalculator />} />
-          <Route path="/historic" element={<HistoricCurrency />} />
-        </Routes>
+        <CurrencyCalculator />
       </Stack>
     </ChakraProvider>
   );
