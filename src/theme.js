86beb7c77266c1dd71f0ee470860219d3b374ce6
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-09-15 17:57:02 -0300

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+  colors: {
+    primary: {
+      100: "#f7fafc",
+      // ...
+      900: "#1a202c",
+    },
+  },
+})
+
+export default theme
